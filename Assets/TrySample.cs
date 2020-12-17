446c5958a082946aaf3a01090bf7383e018a3b0b
KenyaW467 <owk63562948@outlook.jp>
2020-12-17 13:49:19 +0900

    Revert "復元"
    This reverts commit d46600f634514f3501147ca9dbbd814e8f3d153e.

---
+++ Assets/TrySample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TrySample : Image, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerEnter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerExit");
+    }
+}
