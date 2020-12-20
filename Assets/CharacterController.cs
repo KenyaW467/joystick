5066d2161a2c2219b97d96a95f8d4324d1dfdd90
KenyaW467 <owk63562948@outlook.jp>
2020-12-20 21:48:49 +0900

    vcz

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -6,48 +6,51 @@ public class CharacterController : MonoBehaviour
 {
     public float speed = 0.01f;
     public bool pc_mode = false;
-    //public VariableJoystick variableJoystick;
+
     public FloatingJoystick variableJoystick;
-    public Rigidbody2D rb;
+
+    //gemeover画面
+    public GameObject tauch_wall;
+
+    void Start()
+    {
+        tauch_wall = GameObject.Find("DirectorScript");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (pc_mode == false)
+        if (tauch_wall.GetComponent<DirectorScript>().pause_flg != true)
         {
-            //Vector3 pos = transform.position;
-            //pos.x += variableJoystick.Horizontal * speed;
-            //pos.y += variableJoystick.Vertical * speed;
-            transform.Translate(variableJoystick.Horizontal * speed, variableJoystick.Vertical * speed, 0);
-        }
-        else
-        {
+            /*キャラクターの移動制御*/
             if (Input.GetKey("up"))
             {
-                //transform.position += transform.forward * speed * 50 * Time.deltaTime;
                 transform.Translate(0, 1 * speed, 0);
             }
-            if (Input.GetKey("down"))
+            else if (Input.GetKey("down"))
             {
-                //transform.position -= transform.forward * speed * 50 * Time.deltaTime;
                 transform.Translate(0, -1 * speed, 0);
             }
-            if (Input.GetKey("right"))
+            else if (Input.GetKey("right"))
             {
-                //transform.position += transform.right * speed * 50 * Time.deltaTime;
                 transform.Translate(1 * speed, 0, 0);
             }
-            if (Input.GetKey("left"))
+            else if (Input.GetKey("left"))
             {
-                //transform.position -= transform.right * speed * 50 * Time.deltaTime;
                 transform.Translate(-1 * speed, 0, 0);
             }
+            else
+            {
+                transform.Translate(variableJoystick.Horizontal * speed, variableJoystick.Vertical * speed, 0);
+            }
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.2f, 2.2f),
+            Mathf.Clamp(transform.position.y, 0.0f, 4.6f), 0);
         }
-
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("何かにあたった！");
+        tauch_wall.GetComponent<DirectorScript>().gameover_window();
     }
+
 }
